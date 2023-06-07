5385b9bd65264a9168387d9d946b1f1ca4bb7924
Inaro <59608378+inaro8@users.noreply.github.com>
2023-06-07 11:39:08 +0900

    作成

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+const header = document.querySelector('.js-header');
+const kv = document.querySelector('.js-kv');
+const headerHeight = header.clientHeight;
+const kvHeight = kv.clientHeight;
+let startPos = 0;
+let scrollTop;
+
+	window.addEventListener('scroll', function () {
+		scrollTop = window.pageYOffset;
+
+		if (scrollTop > headerHeight) {
+			header.classList.add('is-hide');
+		} else {
+			header.classList.remove('is-hide');
+		}
+
+		// 下にスクロールしたとき
+		if (scrollTop > startPos) {
+			header.classList.remove('is-show');
+		}
+		// 上にスクロールしたとき
+		else {
+			if (scrollTop < kvHeight) {
+				header.classList.remove('is-show');
+			} else {
+				header.classList.add('is-show');
+			}
+		}
+		startPos = scrollTop;
+	});
