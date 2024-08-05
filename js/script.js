b17a176d748a24cd49673e0bfc66ecc67ce4905c
Tusch <59608378+tusch8@users.noreply.github.com>
2024-08-05 19:19:52 +0900

    初期表示はabsoluteで追従時にfixedになるよう修正

--- js/script.js
+++ js/script.js
@@ -1,31 +1,64 @@
-
-const header = document.querySelector('.js-header');
-const kv = document.querySelector('.js-kv');
+const header = document.querySelector("[data-header]");
+const kv = document.querySelector("[data-header-hide]");
 const headerHeight = header.clientHeight;
 const kvHeight = kv.clientHeight;
+
+const FIXED_CLASS = "is-fixed";
+const SHOW_CLASS = "is-show";
+const HIDE_CLASS = "is-hide";
+
 let startPos = 0;
 let scrollTop;
 
-	window.addEventListener('scroll', function () {
-		scrollTop = window.scrollY;
+window.addEventListener("scroll", function () {
+	scrollTop = window.scrollY;
 
+	// 下にスクロールしたとき
+	if (scrollTop > startPos) {
+		header.classList.remove(SHOW_CLASS);
+
+		// ヘッダーの高さよりスクロールしたとき
 		if (scrollTop > headerHeight) {
-			header.classList.add('is-hide');
+			header.classList.add(FIXED_CLASS);
+
+			// ヘッダーより上
 		} else {
-			header.classList.remove('is-hide');
+			header.classList.remove(FIXED_CLASS);
 		}
 
-		// 下にスクロールしたとき
-		if (scrollTop > startPos) {
-			header.classList.remove('is-show');
+		// キービジュアルより上だったら
+		if (scrollTop < kvHeight) {
+			header.classList.remove(HIDE_CLASS);
 		}
-		// 上にスクロールしたとき
+
+		// キービジュアルより下だったら
 		else {
-			if (scrollTop < kvHeight) {
-				header.classList.remove('is-show');
-			} else {
-				header.classList.add('is-show');
-			}
+			header.classList.add(HIDE_CLASS);
 		}
-		startPos = scrollTop;
-	});
+
+		// 上にスクロールしたとき
+	} else {
+		// ヘッダーの高さよりスクロールしたとき
+		if (scrollTop > headerHeight) {
+			header.classList.add(FIXED_CLASS);
+
+			// キービジュアルより上だったら
+			if (scrollTop < kvHeight) {
+				header.classList.remove(SHOW_CLASS);
+				header.classList.add(HIDE_CLASS);
+			}
+
+			// キービジュアルより下だったら
+			else {
+				header.classList.remove(HIDE_CLASS);
+				header.classList.add(SHOW_CLASS);
+			}
+
+			// ヘッダーより上
+		} else {
+			header.classList.remove(FIXED_CLASS, SHOW_CLASS, HIDE_CLASS);
+		}
+	}
+
+	startPos = scrollTop;
+});
